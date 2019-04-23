b9f5248df05ec3c48cf6f2b777cab226ee5718fd
allan <allancruz716@gmail.com>
2019-04-22 22:57:15 -0300

    Definir versões e itens de série

--- src/components/home/versions/Versions.vue
+++ src/components/home/versions/Versions.vue
@@ -9,14 +9,27 @@
                     <b-tabs pills card vertical>
                         <b-tab title="A 200" active>
                             <b-card-text>
-                                <div class="info-cars">
+                                <div class="versions-info">
                                     <img src="../../../assets/versoes/a-200.png" alt="">
 
-                                    <h3>Itens de série</h3>
-                                    <ul>
-                                        <li>Ar condicionado</li>
-                                    </ul>
-                                    <div class="info-general absolute right top">
+                                    <div class="versions-items">
+                                        <h3>Itens de série</h3>
+                                        <ul>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                        </ul>
+                                    </div>
+                                    <div class="versions-general absolute right top">
                                         <h3>Informações Gerais</h3>
                                         <ul>
                                             <li>Cilindros/válvulas por cilindro</li>
@@ -26,22 +39,33 @@
                                             <li>em rev/min:</li>
                                             <li>Torque máximo EEC - Nm (libra-pé):</li>
                                         </ul>
-
-
-
-
                                     </div>
                                 </div>
                             </b-card-text>
                         </b-tab>
                         <b-tab title="A 250 turbo sport">
                             <b-card-text>
-                                <div class="info-cars">
-                                    <h3>Itens de série</h3>
-                                    <ul>
-                                        <li>Ar condicionado</li>
-                                    </ul>
-                                    <div class="info-general absolute right top">
+                                <div class="versions-info">
+                                    <img src="../../../assets/versoes/GLC_250.png" alt="">
+
+                                    <div class="versions-items">
+                                        <h3>Itens de série</h3>
+                                        <ul>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                        </ul>
+                                    </div>
+                                    <div class="versions-general absolute right top">
                                         <h3>Informações Gerais</h3>
                                         <ul>
                                             <li>Cilindros/válvulas por cilindro</li>
@@ -51,22 +75,33 @@
                                             <li>em rev/min:</li>
                                             <li>Torque máximo EEC - Nm (libra-pé):</li>
                                         </ul>
-
-
-
-
                                     </div>
                                 </div>
                             </b-card-text>
                         </b-tab>
                         <b-tab title="A 45 amg 4matic">
                             <b-card-text>
-                                <div class="info-cars">
-                                    <h3>Itens de série</h3>
-                                    <ul>
-                                        <li>Ar condicionado</li>
-                                    </ul>
-                                    <div class="info-general absolute right top">
+                                <div class="versions-info">
+                                    <img src="../../../assets/versoes/gla_vus_032_bc.png" alt="">
+
+                                    <div class="versions-items">
+                                        <h3>Itens de série</h3>
+                                        <ul>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                            <li>Ar condicionado</li>
+                                            <li>AirBag</li>
+                                            <li>Desenbaçador traseiro</li>
+                                            <li>Ar quente</li>
+                                            <li>Freios ABS</li>
+                                            <li>Teto solar</li>
+                                        </ul>
+                                    </div>
+                                    <div class="versions-general absolute right top">
                                         <h3>Informações Gerais</h3>
                                         <ul>
                                             <li>Cilindros/válvulas por cilindro</li>
