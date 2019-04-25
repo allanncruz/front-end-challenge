943d7f992f547a2704ad3d7a49730e153ba056ec
Allan Dantas <allancruz716@gmail.com>
2019-04-25 14:38:55 -0300

    Definir base rodape

--- src/components/shared/footer/Footer.vue
+++ src/components/shared/footer/Footer.vue
@@ -1,5 +1,21 @@
 <template>
-<div></div>
+    <div>
+        <div class="footer">
+            <b-container>
+                <b-row>
+                    <b-col cols="2">
+                        <img src="../../../assets/logo.png" alt="Grand Motors" >
+                    </b-col>
+                    <b-col cols="8">
+                        © Copyright 2015 - Cliente - Todos os direitos reservados.
+                    </b-col>
+                    <b-col cols="2">
+
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+    </div>
 </template>
 
 <script>
